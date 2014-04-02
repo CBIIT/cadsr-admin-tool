d5a2a2614ed1c161e67c2cfc7a4ebb57ac463695
unknown <konkapv@NCI-01874632-L.nci.nih.gov>
2014-04-02 08:52:18 -0400

    Fix for CADSRADMIN-7

--- evsbrowser/src/java/gov/nih/nci/ncicb/cadsr/admintool/struts/action/EVSSearch.java
+++ evsbrowser/src/java/gov/nih/nci/ncicb/cadsr/admintool/struts/action/EVSSearch.java
@@ -23,9 +23,9 @@ public class EVSSearch {
 
 	public static String CACORE_SERVICE_URL;
 
-	public static List searchEVS(String term, String dtsVocab, String retired, 
-			String sSearchInEVS, String sUISearchType, 
-			String sMetaSource, 
+	public static List searchEVS(String term, String dtsVocab, String retired,
+			String sSearchInEVS, String sUISearchType,
+			String sMetaSource,
 			int sMetaLimit) throws Exception {
 
 		String algorithm;
@@ -47,14 +47,14 @@ public class EVSSearch {
 		List vMetaDefs = new ArrayList();
 		//List vMetaDefs = null;
 
-		if (dtsVocab.equals("Thesaurus/Metathesaurus") || 
-				dtsVocab.equals("") || dtsVocab.equals("NCI Thesaurus") || 
+		if (dtsVocab.equals("Thesaurus/Metathesaurus") ||
+				dtsVocab.equals("") || dtsVocab.equals("NCI Thesaurus") ||
 				dtsVocab.equals("NCI_Thesaurus")) {
 			dtsVocab = "NCI Thesaurus";
 			altNameType = "NCI_CONCEPT_CODE";
 		}
 		// for search Meta by (LOINC) code
-		else if (dtsVocab.equals("Metathesaurus") || 
+		else if (dtsVocab.equals("Metathesaurus") ||
 				sSearchInEVS.equals("Code")) {
 			dtsVocab = "NCI_Thesaurus";
 			altNameType = "NCI_CONCEPT_CODE";
@@ -66,28 +66,29 @@ public class EVSSearch {
 			altNameType = "UMLS_CUI";
 			isMetaCodeSearch = false;
 			sUISearchType = "term";
-		} 
+		}
 		else
 			altNameType = "";
 
 
-		LexEVSApplicationService evsService = 
+		LexEVSApplicationService evsService =
 			(LexEVSApplicationService)ApplicationServiceProvider.getApplicationService("EvsServiceInfo" );
 
 		/*  if (!sSearchInEVS.equals("Concept Code") || term.equals("")) {*/
 		Pattern pattern = Pattern.compile("^C\\d{2,8}");
 	    Matcher matcher = pattern.matcher(term);
-	    	
+
 	    algorithm = getAlgorithm(term);
 	    term = cleanTerm(term);
-	    
+		int totalReturnCount = 0;
+
 		try {
 			if (retired.equals("Include")) // do this if all concepts, including retired, should be included
 				isRetired = new Boolean(false);
 			else {
 
-				try {			
-					
+				try {
+
 					ResolvedConceptReferenceList rcrl = searchPrefTerm(evsService, dtsVocab, term, sMetaLimit, algorithm);
 					if (rcrl != null && rcrl.getResolvedConceptReferenceCount() >0)
 						isRetired = (Boolean) (!rcrl.getResolvedConceptReference(0).getEntity().isIsActive());
@@ -98,6 +99,7 @@ public class EVSSearch {
 
 			}
 
+
 			if (isRetired.equals(bFalse)) {
 				ResolvedConceptReferenceList concepts = null;
 				//CCode = term;
@@ -105,20 +107,20 @@ public class EVSSearch {
 				try {
 					if (sSearchInEVS.equals("Concept Code") || matcher.matches())
 						concepts = searchConceptCode(evsService, dtsVocab, term, sMetaLimit);
-					else 
+					else
 						concepts = searchPrefTerm(evsService, dtsVocab, term, sMetaLimit, algorithm);
-					
+
 					codesSize = concepts.getResolvedConceptReferenceCount();
 
 				} catch (Exception ex) {
-					System.out.println("Error do_EVSSearch get concept: " + 
+					System.out.println("Error do_EVSSearch get concept: " +
 							ex.toString());
 				}
 
 				if (concepts != null && codesSize > 0) {
 
 					codeFoundInThesaurus = new Boolean(true);
-					
+
 					prefName = concepts.getResolvedConceptReference(0).getEntityDescription().getContent();
 
 					concepts = new ResolvedConceptReferenceList();
@@ -127,7 +129,7 @@ public class EVSSearch {
 						codesSize = concepts.getResolvedConceptReferenceCount();
 					} catch (Exception ex) {
 						//ex.printStackTrace();
-						System.out.println("Error do_EVSSearch DescLogic: " + 
+						System.out.println("Error do_EVSSearch DescLogic: " +
 								ex.toString());
 					}
 					if (concepts != null) {
@@ -142,16 +144,16 @@ public class EVSSearch {
 								defs = new Definition[0];
 							sDef = sDefDefault; // "No Value Exists.";
 							sDefSrc = "";
-							
+
 							for(int k=0;k< defs.length;k++){
 
 								Definition def = defs[k];
 
 								if (def != null) {
-									if(def.getIsPreferred()){
+									if(def.getIsPreferred() != null){
 										sDef = def.getValue().getContent();
 										if (def.getSourceCount() > 0)
-											sDefSrc = def.getSource(0).getContent(); //get def source 
+											sDefSrc = def.getSource(0).getContent(); //get def source
 									}
 
 								}
@@ -165,6 +167,11 @@ public class EVSSearch {
 							conBean.setSource(sDefSrc);
 							conBean.setType(altNameType);
 							vCon.add(conBean);
+							totalReturnCount++;
+
+							if(totalReturnCount >= sMetaLimit)
+								return vCon;
+
 						}
 					}
 				}
@@ -173,28 +180,32 @@ public class EVSSearch {
 		} catch (Exception ex) {
 			System.out.println("Error do_EVSSearch Concept code: " + ex.toString());
 		}
+
+
 		//Search Meta
 		int length = 0;
 		length = term.length();
 		ResolvedConceptReferenceList concepts = null;
 		if (sSearchInEVS.equals("Concept Code"))
 			concepts = searchConceptCode(evsService, "NCI Metathesaurus", term, sMetaLimit);
-		else if (!sSearchInEVS.equals("Concept Code"))                      
+		else if (!sSearchInEVS.equals("Concept Code"))
 			concepts = searchPrefTerm(evsService, "NCI Metathesaurus", term, sMetaLimit, algorithm);
 
 		if (concepts != null) {
 				ResolvedConceptReference rcr = new ResolvedConceptReference();
 				for (int i = 0; i < concepts.getResolvedConceptReferenceCount(); i++) {
+					if(totalReturnCount >= sMetaLimit)
+						break;
 					rcr = concepts.getResolvedConceptReference(i);
 					prefName = rcr.getEntityDescription().getContent();
 					CCode = rcr.getConceptCode();
-					
+
 					Definition[] defs = rcr.getEntity().getDefinition();
 					if(defs == null)
 						defs = new Definition[0];
 					sDef = sDefDefault; // "No Value Exists.";
 					sDefSrc = "";
-					
+
 					for(int k=0;k< defs.length;k++){
 
 						Definition def = defs[k];
@@ -202,9 +213,9 @@ public class EVSSearch {
 						if (def != null) {
 								sDef = def.getValue().getContent();
 								if (def.getSourceCount() > 0)
-									sDefSrc = def.getSource(0).getContent(); //get def source 
+									sDefSrc = def.getSource(0).getContent(); //get def source
 						}
-						
+
 						EVSBean conBean = new EVSBean();
 						conBean.setCode(CCode);
 						conBean.setDefinition(sDef);
@@ -213,11 +224,13 @@ public class EVSSearch {
 						conBean.setSource(sDefSrc);
 						conBean.setType("UMLS_CUI");
 						vCon.add(conBean);
-						
-					}				
+						totalReturnCount++;
+						if(totalReturnCount >= sMetaLimit)
+							break;
+					}
 				}
 			}
-			
+
 		evsService = null;
 		System.out.println(vCon.size());
 		return vCon;
@@ -229,14 +242,14 @@ public class EVSSearch {
 
 		int codesSize = 0;
 		try {
-			CodedNodeSet metaNodes = evsService.getNodeSet(dtsVocab, null, null); 
+			CodedNodeSet metaNodes = evsService.getNodeSet(dtsVocab, null, null);
 
 			ConceptReferenceList crefs = ConvenienceMethods.
 			createConceptReferenceList(new String[]{CCode}, dtsVocab);
 			metaNodes = metaNodes.restrictToCodes(crefs);
 
 			metaNodes.restrictToStatus(ActiveOption.ACTIVE_ONLY, null);
-			
+
 			concepts = metaNodes.resolveToList(
 					null, //Sorts used to sort results (null means sort by match score)
 					null, //PropertyNames to resolve (null resolves all)
@@ -247,7 +260,7 @@ public class EVSSearch {
 			codesSize = concepts.getResolvedConceptReferenceCount();
 
 		} catch (Exception ex) {
-			System.out.println("Error do_EVSSearch get concept: " + 
+			System.out.println("Error do_EVSSearch get concept: " +
 					ex.toString());
 		}
 		return concepts;
@@ -260,15 +273,15 @@ public class EVSSearch {
 		ResolvedConceptReferenceList concepts = new ResolvedConceptReferenceList();
 		int codesSize = 0;
 		try {
-			CodedNodeSet metaNodes = evsService.getNodeSet(dtsVocab, null, null); 
+			CodedNodeSet metaNodes = evsService.getNodeSet(dtsVocab, null, null);
 
-			metaNodes = metaNodes.restrictToMatchingDesignations(prefName, //the text to match 
+			metaNodes = metaNodes.restrictToMatchingDesignations(prefName, //the text to match
 					CodedNodeSet.SearchDesignationOption.ALL,  //whether to search all designation, only Preferred or only Non-Preferred
 					algorithm, //the match algorithm to use
 					null); //the language to match (null matches all)
 
 			metaNodes = metaNodes.restrictToStatus(ActiveOption.ACTIVE_ONLY, null);
-			
+
 			concepts = metaNodes.resolveToList(
 					null, //Sorts used to sort results (null means sort by match score)
 					null, //PropertyNames to resolve (null resolves all)
@@ -278,7 +291,7 @@ public class EVSSearch {
 			codesSize = concepts.getResolvedConceptReferenceCount();
 		} catch (Exception ex) {
 			//ex.printStackTrace();
-			System.out.println("Error do_EVSSearch DescLogic: " + 
+			System.out.println("Error do_EVSSearch DescLogic: " +
 					ex.toString());
 		}
 
@@ -305,7 +318,7 @@ public class EVSSearch {
 
 			}
 		} catch (Exception ee) {
-			System.err.println("problem in Thesaurus syn EVSSearch-getSource: " + 
+			System.err.println("problem in Thesaurus syn EVSSearch-getSource: " +
 					ee);
 			return source;
 		}
@@ -331,7 +344,7 @@ public class EVSSearch {
 					definition = termStr.substring(iStartDef, iEndDef);
 			}
 		} catch (Exception ee) {
-			System.err.println("problem in Thesaurus syn EVSSearch-getDefinition: " + 
+			System.err.println("problem in Thesaurus syn EVSSearch-getDefinition: " +
 					ee);
 			return definition;
 		}
@@ -342,9 +355,9 @@ public class EVSSearch {
 		LexEVSApplicationService evsService = null;
 
 		try {
-			evsService = 
+			evsService =
 				(LexEVSApplicationService)ApplicationServiceProvider.getApplicationService("EvsServiceInfo");
-			
+
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
@@ -352,23 +365,23 @@ public class EVSSearch {
 		if (dtsVocab == null)
 			dtsVocab = "";
 		String CCode = "";
-		if (dtsVocab.equals("Thesaurus/Metathesaurus") || 
-				dtsVocab.equals("") || dtsVocab.equals("NCI Thesaurus") || 
+		if (dtsVocab.equals("Thesaurus/Metathesaurus") ||
+				dtsVocab.equals("") || dtsVocab.equals("NCI Thesaurus") ||
 				dtsVocab.equals("NCI_Thesaurus"))
 			dtsVocab = "NCI_Thesaurus";
 		ResolvedConceptReferenceList codes2 = null;
 		int codesSize = 0;
 
 		try {
-			CodedNodeSet metaNodes = evsService.getNodeSet("NCI MetaThesaurus", null, null); 
+			CodedNodeSet metaNodes = evsService.getNodeSet("NCI MetaThesaurus", null, null);
 
-			metaNodes = metaNodes.restrictToMatchingDesignations(prefName, //the text to match 
+			metaNodes = metaNodes.restrictToMatchingDesignations(prefName, //the text to match
 					CodedNodeSet.SearchDesignationOption.PREFERRED_ONLY,  //whether to search all designation, only Preferred or only Non-Preferred
 					"exactMatch", //the match algorithm to use
 					null); //the language to match (null matches all)
 
 			metaNodes = metaNodes.restrictToStatus(ActiveOption.ACTIVE_ONLY, null);
-			
+
 			codes2 = metaNodes.resolveToList(
 					null, //Sorts used to sort results (null means sort by match score)
 					null, //PropertyNames to resolve (null resolves all)
@@ -391,7 +404,7 @@ public class EVSSearch {
 
 			}
 		}
-		
+
 		evsService = null;
 		return CCode;
 	}
@@ -402,11 +415,11 @@ public class EVSSearch {
 		try {
 
 			EVSSearch evsSearch = new EVSSearch();
-			List<EVSBean> concept = 
-				(List<EVSBean>)evsSearch.searchEVS("B*" +
-						"", "NCI_Thesaurus", 
+			List<EVSBean> concept =
+				(List<EVSBean>)evsSearch.searchEVS("gene*" +
+						"", "NCI_Thesaurus",
 						"Include", "" +
-						"Synonym", 
+						"Synonym",
 						"term", "All Sources", 100);
 			for (EVSBean b: concept) {
 				System.out.println("-----");
@@ -427,27 +440,27 @@ public class EVSSearch {
 	}
 
 private static String getAlgorithm(String termStr) {
-		
+
 		String algorithm = "exactMatch";
-		
+
 		boolean starts = false;
 		boolean ends = false;
 		boolean contains = false;
 		boolean multiple = false;
 		boolean embed = false;
-		
+
 		termStr = termStr.trim();
 		ends = termStr.startsWith("*"); // Term ends with rest of the string
 		starts = termStr.endsWith("*"); // Term starts with rest of the string
-		
+
 		contains = termStr.substring(1,termStr.length()-1).indexOf(" *") >= 0 ||
 					termStr.substring(1,termStr.length()-1).indexOf("* ") >= 0;
-		if (!contains)		
+		if (!contains)
 			embed = termStr.substring(1,termStr.length()-1).indexOf("*") >= 0;
-		
+
 		multiple = termStr.indexOf(' ') > 0;
-		
-		
+
+
 		if (starts)
 			algorithm = "startsWith";
 		if (contains || ends)
@@ -456,11 +469,11 @@ private static String getAlgorithm(String termStr) {
 			algorithm = "nonLeadingWildcardLiteralSubString";
 		if (multiple && starts && ends && contains)
 			algorithm = "contains";
-		
+
 		return algorithm;
 	}
-	
-	private static String cleanTerm(String termStr) {		
+
+	private static String cleanTerm(String termStr) {
 		termStr = termStr.trim();
 		termStr = termStr.replace("*","");
 		return termStr;
